3f9ec0be8a47465de5b1f8ba39dc2f60051476c9
OvErDz <41547554+OvErDz@users.noreply.github.com>
2018-07-27 12:08:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+https://cdn.discordapp.com/attachments/456656241775738911/472355947981963285/images.jpgconst Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
@@ -76,7 +76,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-					.setFooter(`Fox Bot`, 'https://cdn.discordapp.com/attachments/470892033612185610/471340486808961024/download.jpg')
+					.setFooter(`Fox Bot`, 'https://cdn.discordapp.com/attachments/456656241775738911/472355947981963285/images.jpg')
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
